d5b925574ff85294c55a5e27be98a37f710a77cb
Pantkowsky <a.pantkowski@gmail.com>
2020-04-27 00:49:45 +0200

    Implemented the initial setup for http calls

--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,67 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Secrets exposing (tokenCircleCI)
+import Html exposing (div, img, button, Html, text, h1)
 import Html.Attributes exposing (src)
-
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode exposing (Decoder, map, field, string)
 
 ---- MODEL ----
 
 
-type alias Model =
-    {}
+type alias Model = {response : Response}
+type alias Me = { login: String }
+
+type Response = Initial
+    | Loading
+    | Failure
+    | Success String
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( initModel, Cmd.none )
 
-
+initModel : Model
+initModel = Model Initial
 
 ---- UPDATE ----
 
 
-type Msg
-    = NoOp
+type Msg = NoOp
+    | Fetch
+    | ApiData (Result Http.Error Me)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        NoOp -> ( { model | response = Initial }, Cmd.none )
+        Fetch -> ( { model | response = Loading }, fetchData )
+        ApiData data ->
+            case data of
+                Ok d -> ( { model | response = Success d.login }, Cmd.none )
+                Err _ -> ( { model | response = Failure }, Cmd.none )
+
+fetchData : Cmd Msg
+fetchData =
+    Http.get
+        { url = requestUrl
+        , expect = Http.expectJson ApiData decoder}
 
 
+decoder : Decoder Me
+decoder =
+    map Me
+        (field "login" string)
+
+requestUrl : String
+requestUrl = String.concat["https://circleci.com/api/v1.1/me", apiToken]
+
+apiToken : String
+apiToken = String.concat["?circle-token=", tokenCircleCI]
 
 ---- VIEW ----
 
@@ -38,10 +70,17 @@ view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        , button [ onClick Fetch ] [ text "Fetch data" ]
+        , me model.response
         ]
 
-
+me : Response -> Html Msg
+me res =
+    case res of
+        Initial -> h1 [] [ text "Initial" ]
+        Loading -> h1 [] [ text "Loading" ]
+        Failure -> h1 [] [ text "Error" ]
+        Success s -> h1 [] [ text s]
 
 ---- PROGRAM ----
 
