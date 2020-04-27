c299f91c3fcd2543df011c41bf845bba6fe1ffa3
Pantkowsky <a.pantkowski@gmail.com>
2020-04-27 21:57:27 +0200

    Extended the page with `email` data

--- src/Main.elm
+++ src/Main.elm
@@ -2,22 +2,32 @@ module Main exposing (..)
 
 import Browser
 import Secrets exposing (tokenCircleCI)
-import Html exposing (div, img, button, Html, text, h1)
-import Html.Attributes exposing (src)
+import Html exposing (div, li, button, Html, text, h1)
 import Html.Events exposing (onClick)
 import Http
-import Json.Decode exposing (Decoder, map, field, string)
+import Json.Decode exposing (Decoder, map2, field, string)
+
+---- PROGRAM ----
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
 
 ---- MODEL ----
 
-
-type alias Model = {response : Response}
-type alias Me = { login: String }
+type alias Model = { response : Response }
+type alias Me = { login: String, email: String }
 
 type Response = Initial
     | Loading
     | Failure
-    | Success String
+    | Success String String
 
 
 init : ( Model, Cmd Msg )
@@ -29,20 +39,17 @@ initModel = Model Initial
 
 ---- UPDATE ----
 
-
-type Msg = NoOp
-    | Fetch
+type Msg = Fetch
     | ApiData (Result Http.Error Me)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp -> ( { model | response = Initial }, Cmd.none )
         Fetch -> ( { model | response = Loading }, fetchData )
         ApiData data ->
             case data of
-                Ok d -> ( { model | response = Success d.login }, Cmd.none )
+                Ok d -> ( { model | response = Success d.login d.email }, Cmd.none )
                 Err _ -> ( { model | response = Failure }, Cmd.none )
 
 fetchData : Cmd Msg
@@ -54,8 +61,9 @@ fetchData =
 
 decoder : Decoder Me
 decoder =
-    map Me
+    map2 Me
         (field "login" string)
+        (field "selected_email" string)
 
 requestUrl : String
 requestUrl = String.concat["https://circleci.com/api/v1.1/me", apiToken]
@@ -69,27 +77,29 @@ apiToken = String.concat["?circle-token=", tokenCircleCI]
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , button [ onClick Fetch ] [ text "Fetch data" ]
+        [
+        button [ onClick Fetch ] [ text "Fetch data" ]
         , me model.response
         ]
 
 me : Response -> Html Msg
 me res =
     case res of
-        Initial -> h1 [] [ text "Initial" ]
+        Initial -> h1 [ ] [ text "Initial" ]
         Loading -> h1 [] [ text "Loading" ]
         Failure -> h1 [] [ text "Error" ]
-        Success s -> h1 [] [ text s]
+        Success l e -> li [] [
+            h1 [] [ text (formatLogin l) ]
+            , h1 [] [text (formatEmail e)]
+            ]
 
----- PROGRAM ----
+formatLogin : String -> String
+formatLogin login =
+    String.concat["login: ", login]
+
+formatEmail : String -> String
+formatEmail email =
+    String.concat["email: ", email]
 
 
-main : Program () Model Msg
-main =
-    Browser.element
-        { view = view
-        , init = \_ -> init
-        , update = update
-        , subscriptions = always Sub.none
-        }
+
