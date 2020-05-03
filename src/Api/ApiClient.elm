9333965c671938bb08faea634068d1223af4e2c6
Pantkowsky <a.pantkowski@gmail.com>
2020-05-04 00:36:47 +0200

    Added models and query to fetch jobs metadata per branch

--- src/Api/ApiClient.elm
+++ src/Api/ApiClient.elm
@@ -1,6 +1,6 @@
 module Api.ApiClient exposing (..)
 
-import Json.Decode exposing (Decoder, field, map4, string, int)
+import Json.Decode as JD exposing (Decoder, field, map, map2, map4, string, int)
 import Http
 import Secrets exposing (tokenCircleCI)
 
@@ -12,14 +12,29 @@ type alias Me =
     email: String
     }
 
+type alias Build =
+    {
+    num: Int,
+    time: Int
+    }
+
 type Msg = Fetch
-    | ApiData (Result Http.Error Me)
+    | MeData (Result Http.Error Me)
+    | BuildData (Result Http.Error (List Build))
 
 fetchData : Cmd Msg
 fetchData =
     Http.get
         { url = requestUrl
-        , expect = Http.expectJson ApiData decoder}
+        , expect = Http.expectJson MeData decoder}
+
+fetchBuilds : Cmd Msg
+fetchBuilds =
+    Http.get
+    {
+    url = buildsUrl
+    , expect = Http.expectJson BuildData buildListDecoder
+    }
 
 
 decoder : Decoder Me
@@ -30,8 +45,24 @@ decoder =
         (field "login" string)
         (field "selected_email" string)
 
+buildListDecoder : Decoder (List Build)
+buildListDecoder =
+    JD.list buildDecoder
+
+buildDecoder : Decoder Build
+buildDecoder =
+    map2 Build
+        (field "build_num" int)
+        (field "build_time_millis" int)
+
 requestUrl : String
 requestUrl = String.concat["https://circleci.com/api/v1.1/me", appendApiToken]
 
+buildsUrl : String
+buildsUrl = String.concat["https://circleci.com/api/v1.1/project/gh/undo-insurance/android/tree/modularization", appendBuildSuffix]
+
 appendApiToken : String
 appendApiToken = String.concat["?circle-token=", tokenCircleCI]
+
+appendBuildSuffix : String
+appendBuildSuffix = String.concat[appendApiToken, "&limit=50"]
