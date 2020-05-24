a248d8bd9e88074a14f5e4eb993b82cb35ab43de
Pantkowsky <a.pantkowski@gmail.com>
2020-05-24 22:26:07 +0200

    Extracted and renamed `JobData` and `Job` types along with helper methods

--- src/Api/Models.elm
+++ src/Api/Models.elm
@@ -1,6 +1,8 @@
-module Models exposing (..)
+module Models exposing (Build, JobData, Job, Msg(..), parseData)
 
 import Http
+import Set
+import Time
 
 type alias Build =
     {
@@ -10,5 +12,35 @@ type alias Build =
     status: String
     }
 
+type alias JobData =
+    {
+    branches: List String,
+    totalTime: Int,
+    jobs: List Build
+    }
+
+type alias Job =
+    {
+    id: Float,
+    time: Time.Posix
+    }
+
 type Msg = Data (Result Http.Error (List Build))
     | Hover (List Build)
+
+parseData : (List String, Int, List Build) -> List Build -> JobData
+parseData (branches, totalTime, jobs) data =
+    { branches = branches ++ parseBranches data , totalTime = totalTime + parseTotalTime data, jobs = jobs ++ data }
+
+parseBranches: List Build -> List String
+parseBranches data =
+    data
+        |> List.map (\b -> b.branch)
+        |> Set.fromList
+        |> Set.toList
+
+parseTotalTime: List Build -> Int
+parseTotalTime data =
+    data
+        |> List.map (\d -> d.time)
+        |> List.foldl (+) 0
